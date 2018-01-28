6edada89baf3b1d7c2b870c0bbd24c4a073bf1e4
athensbird <popularwatson@gmail.com>
2018-01-28 09:41:58 -0600

    add input validation

--- searchHelper.js
+++ searchHelper.js
@@ -26,54 +26,44 @@ function search() {
           image_url: recipe.image_url
         }
       });
-      for (let i = 0; i < 6; i+=2) {
+      if (recipeList.length !== 0) {
+        $("#recipe-results").empty();
+        for (let i = 0; i < 6; i+=2) {
+          const title = recipeList[i].title;
+          const image_url = recipeList[i].image_url;
+          const url = recipeList[i].url;
+          const author = recipeList[i].author;
+          const recipe_id = recipeList[i].recipe_id;
 
-        const title = recipeList[i].title;
-        const image_url = recipeList[i].image_url;
-        const url = recipeList[i].url;
-        const author = recipeList[i].author;
-        const recipe_id = recipeList[i].recipe_id;
+          const title2 = recipeList[i+1].title;
+          const image_url2 = recipeList[i+1].image_url;
+          const url2 = recipeList[i+1].url;
+          const author2 = recipeList[i+1].author;
+          const recipe_id2 = recipeList[i+1].recipe_id;
 
-        const title2 = recipeList[i+1].title;
-        const image_url2 = recipeList[i+1].image_url;
-        const url2 = recipeList[i+1].url;
-        const author2 = recipeList[i+1].author;
-        const recipe_id2 = recipeList[i+1].recipe_id;
-
-        const carouselItem = `
-          <div class = "container">
-            <div class = "row">
-              <div class = "col-md-6 col-xs-6">
-                <img height="200" width="300" src="${image_url}"  style="margin:7px 0px;" />
-                <h4><a href="${url}" class="recipe" style = "color:red; width:80%; background-color:black; color:white; text-align:center; padding:10px; margin-top:15px;">${title}</a>
-                <button class="btn add-more" onclick="save('${title}', '${url}', '${author}', '${recipe_id}', '${image_url}')">+</button></h4>
+          const carouselItem = `
+            <div class = "container">
+              <div class = "row">
+                <div class = "col-md-6 col-xs-6">
+                  <img height="200" width="300" src="${image_url}"  style="margin:7px 0px;" />
+                  <h4><a href="${url}" class="recipe" style = "color:red; width:80%; background-color:black; color:white; text-align:center; padding:10px; margin-top:15px;">${title}</a>
+                  <button class="btn add-more" onclick="save('${title}', '${url}', '${author}', '${recipe_id}', '${image_url}')">+</button></h4>
+                </div>
+                <div class = "col-md-6 col-xs-6">
+                  <img height="200" width="300" src="${image_url2}" style="margin:7px 0px;"/>
+                  <h4> <a href="${url2}" class="recipe" style = "color:red; width:80%; margin-top:15px; background-color:black; color:white; text-align:center; padding:10px;">${title2}</a>
+                  <button class="btn add-more" onclick="save('${title2}', '${url2}', '${author2}', '${recipe_id2}', '${image_url2}')">+</button></h4>
+                </div>
               </div>
-              <div class = "col-md-6 col-xs-6">
-                <img height="200" width="300" src="${image_url2}" style="margin:7px 0px;"/>
-                <h4> <a href="${url2}" class="recipe" style = "color:red; width:80%; margin-top:15px; background-color:black; color:white; text-align:center; padding:10px;">${title2}</a>
-                <button class="btn add-more" onclick="save('${title2}', '${url2}', '${author2}', '${recipe_id2}', '${image_url2}')">+</button></h4>
+              <div class = "row">
               </div>
-            </div>
-            <div class = "row">
-            </div>
-         </div>`
-        $("#recipe-results").append(carouselItem);
+           </div>`
+          $("#recipe-results").append(carouselItem);
+        }
+      } else {
+        $("#recipe-results").empty();
+        $("#recipe-results").append("<h3>No recipe found!</h3>");
       }
-      /*
-      for (let i = 0; i < 5; i+=2) {
-        const title = recipeList[i].title;
-        const image_url = recipeList[i].image_url;
-        const url = recipeList[i].url;
-        const author = recipeList[i].author;
-        const recipe_id = recipeList[i].recipe_id;
-        const carouselItem = `<div>
-          <a href="${url}">${title}</a>
-          <img height="200" width="300" src="${image_url}"/>
-          <button class="button" onclick="save('${title}', '${url}', '${author}', '${recipe_id}', '${image_url}')">Save to Favorites</button>
-        </div><br/>`
-        $("#recipe-results").append(carouselItem);
-      }
-      */
     }
   });
 }
