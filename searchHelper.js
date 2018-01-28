81ad66351876a1356f3436dad134189602beb3c4
athensbird <popularwatson@gmail.com>
2018-01-28 08:28:22 -0600

    add/remove buttons

--- searchHelper.js
+++ searchHelper.js
@@ -26,6 +26,40 @@ function search() {
           image_url: recipe.image_url
         }
       });
+      for (let i = 0; i < 6; i+=2) {
+
+        const title = recipeList[i].title;
+        const image_url = recipeList[i].image_url;
+        const url = recipeList[i].url;
+        const author = recipeList[i].author;
+        const recipe_id = recipeList[i].recipe_id;
+
+        const title2 = recipeList[i+1].title;
+        const image_url2 = recipeList[i+1].image_url;
+        const url2 = recipeList[i+1].url;
+        const author2 = recipeList[i+1].author;
+        const recipe_id2 = recipeList[i+1].recipe_id;
+
+        const carouselItem = `
+          <div class = "container">
+            <div class = "row">
+              <div class = "col-md-6 col-xs-6">
+                <h4><a href="${url}" style = "color:red; width:80%; background-color:black; color:white; text-align:center; padding:10px; margin-top:15px;">${title}</a>
+                <button class="btn add-more" onclick="save('${title}', '${url}', '${author}', '${recipe_id}', '${image_url}')">+</button></h4>
+                <img height="200" width="300" src="${image_url}"  style="margin:7x 0px;" />
+              </div>
+              <div class = "col-md-6 col-xs-6">
+                <h4> <a href="${url2}" style = "color:red; width:80%;margin-top:5px; background-color:black; color:white; text-align:center; padding:10px;">${title2}</a>
+                <button class="btn add-more" onclick="save('${title2}', '${url2}', '${author2}', '${recipe_id2}', '${image_url2}')">+</button></h4>
+                <img height="200" width="300" src="${image_url2}" style="margin:7px 0px;"/>
+              </div>
+            </div>
+            <div class = "row">
+            </div>
+         </div>`
+        $("#recipe-results").append(carouselItem);
+      }
+      /*
       for (let i = 0; i < 5; i+=2) {
         const title = recipeList[i].title;
         const image_url = recipeList[i].image_url;
@@ -39,6 +73,7 @@ function search() {
         </div><br/>`
         $("#recipe-results").append(carouselItem);
       }
+      */
     }
   });
 }
@@ -56,7 +91,7 @@ function save(title, url, author, recipe_id, image_url) {
       const recipeItem = `<div id="${rid}">
         <a href="${recipe.url}">${recipe.title}</a>
         <img height="200" width="300" src="${recipe.image_url}"/>
-        <button onclick="remove('${rid}')">Remove</button>
+        <button class="btn remove-me" onclick="remove('${rid}')">-</button>
       </div><br/>`
       $("#favorites").append(recipeItem);
     }
@@ -86,7 +121,7 @@ $(function () {
         const recipeItem = `<div id="${rid}">
           <a href="${recipe.url}">${recipe.title}</a>
           <img height="200" width="300" src="${recipe.image_url}"/>
-          <button onclick="remove('${rid}')">Remove</button>
+          <button class="btn remove-me" onclick="remove('${rid}')">-</button>
         </div><br/>`;
         $favorites.append(recipeItem);
       });
